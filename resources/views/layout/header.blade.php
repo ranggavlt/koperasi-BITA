a319445749ffd384072ceb413128ded915dc8dc5
Made Aditya <madeaditya202@gmail.com>
2026-02-26 11:37:32 +0700

    penambahan tampilan

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,25 @@
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}" />
+  <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}" />
+
+  <title>Soft UI Dashboard Tailwind</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+  <!-- Nucleo Icons -->
+  <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+  <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+
+  <!-- Popper -->
+  <script src="https://unpkg.com/@popperjs/core@2"></script>
+
+  <!-- Main Styling -->
+  <link href="{{ asset('assets/css/soft-ui-dashboard-tailwind.css') }}?v=1.0.5" rel="stylesheet" />
+</head>
